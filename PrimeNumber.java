d65d1408c5b4511edf97a26d6bd13e95c1f3b22e
pooja481-pb <girgaonkarpooja438@gmail.com>
2023-12-19 10:54:38 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ PrimeNumber.java
@@ -0,0 +1,22 @@
+package pack3;
+
+public class PrimeNumber {
+
+        public static void main(String[] args) {
+
+            int n = 100;
+            boolean f = false;
+            for (int i = 2; i <= n / 2; ++i) {
+                if (n % i == 0) {
+                    f = true;
+                    break;
+                }
+            }
+
+            if (!f)
+                System.out.println(n + " is a prime number.");
+            else
+                System.out.println(n + " is not a prime number.");
+        }
+    }
+
